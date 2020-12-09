46cec79b6e05fa393b19d4b40b8feb6d8f5d53bb
joseednaldo <49928456+joseednaldo@users.noreply.github.com>
2020-12-09 00:05:44 -0300

    melhorias

---
+++ Models/ValeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoFacil.Models
+{
+    public class ValeData
+    {
+        public int ValeId { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public int FuncionarioId { get; set; }
+    
+    }
+}
